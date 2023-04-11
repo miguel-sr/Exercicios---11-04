70b64f9e0309ea0eba581252e0371dbac2da03d5
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-11 10:40:24 -0300

    :tada: init

---
+++ Exercicios---11-04/Exercicio_1050.cs
@@ -0,0 +1,51 @@
+﻿namespace Exercicios___11_04
+{
+    public class Exercicio_1050
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu DDD?");
+            var ddd = Console.ReadLine();
+
+            switch(ddd)
+            {
+                case "61":
+                    Console.WriteLine("Brasilia");
+                    break;
+
+                case "71":
+                    Console.WriteLine("Salvador");
+                    break;
+
+                case "11":
+                    Console.WriteLine("Sao Paulo");
+                    break;
+
+                case "21":
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+
+                case "32":
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+
+                case "19":
+                    Console.WriteLine("Campinas");
+                    break;
+
+                case "27":
+                    Console.WriteLine("Vitoria");
+                    break;
+
+                case "31":
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+
+                default:
+                    Console.WriteLine("DDD não encontrado");
+                    break;
+            }
+        }
+            
+    }
+}
