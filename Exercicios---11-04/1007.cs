71160ac22f352c27fcc8f450eff8fc9a8bf055fd
Usuario <Usuario@INVENT003>
2023-04-11 08:50:53 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios---11-04/1007.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicios___11_04
+{
+    public class Exercicios
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
