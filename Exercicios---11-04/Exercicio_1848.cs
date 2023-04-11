54ffa047f6683425a97fc9faac9e229241ceb91f
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-11 13:58:13 -0300

    :sparkles: feat

---
+++ Exercicios---11-04/Exercicio_1848.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercicios___11_04
+{
+    public class Exercicio_1848
+    {
+        public static void Main(string[] args)
+        {
+            int numero = 0;
+            var resultadoLoteria = Array.Empty<int>();
+
+            for (int contadorCawCaw = 0; contadorCawCaw < 3;)
+            {
+                string texto = new string(Console.ReadLine().Reverse().ToArray());
+
+                if (texto == "wac wac")
+                {
+                    resultadoLoteria = resultadoLoteria.Append(numero).ToArray();
+                    numero = 0;
+                    contadorCawCaw++;
+                } else
+                {
+                    for (int posicaoDoCaractere = 0; posicaoDoCaractere < texto.Length; posicaoDoCaractere++)
+                    {
+                        if (texto[posicaoDoCaractere] == '*')
+                        {
+                            numero = numero + (posicaoDoCaractere == 0 ? 1 : posicaoDoCaractere * 2);
+                        }
+                    }
+                }
+            }
+
+            foreach (var item in resultadoLoteria)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
