f63d3e1f3bf2a2b4527550764e4bf25d9a67e2fb
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-11 10:25:01 -0300

    :tada: init

---
+++ Exercicios---11-04/Exercicio_1043.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicios___11_04
+{
+    public class Exercicio_1043
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor para A:");
+            var A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para B:");
+            var B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor para C:");
+            var C = Convert.ToDouble(Console.ReadLine());
+
+            if ((A + B) > C && (A + C) > B && (C + B) > A)
+            {
+                Console.WriteLine("Perimetro = " + Math.Round(A + B + C, 1));
+            } else
+
+                Console.WriteLine("Area = " + Math.Round((A + B) * C / 2, 1));
+            }
+            
+        }
+}
