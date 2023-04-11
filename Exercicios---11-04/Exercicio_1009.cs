cca18832489707433646e8699e9ff977305ece75
Miguel S Ramos <miguelsramos458@gmail.com>
2023-04-11 09:25:54 -0300

    :tada: init

---
+++ Exercicios---11-04/Exercicio_1009.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicios___11_04
+{
+    public class Exercicio_1009
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o nome do funcionário?");
+            var nomeFuncionario = Console.ReadLine();
+
+            Console.WriteLine("Qual o salário fixo deste funcionário?");
+            var salarioFixo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor total de vendas realizada por " + nomeFuncionario + "?");
+            var valorTotalDasVendas = Convert.ToDouble(Console.ReadLine());
+
+            var salarioTotal = salarioFixo + valorTotalDasVendas * 0.15;
+
+            Console.WriteLine("O salário final de " + nomeFuncionario + " é de R$" + Math.Round(salarioTotal, 2));
+
+        }
+    }
+}
